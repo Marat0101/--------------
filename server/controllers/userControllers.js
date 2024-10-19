55d923412248f14346338ec1723ba6b0db48e2e6
Marat0101** <msakirzanov112@gmail.com**>
2024-10-19 09:04:27 +0300

    registr

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,27 +1,8 @@
 const ApiError=require('../error/ApiError');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const {User, Basket}= require('../models/models')
 
 class UserControllers {
-    async registration(req,res, next){
-        const {email, password, role} = req.body
-        if(!email || !password){
-            return next(ApiError.badRequest('Некорректный email или password!'))
-        }
-        const candidate = await User.findOne({where: {email}})
-        if(candidate){
-            return next(ApiError.badRequest('пользователь с таким email уже существует'))
-        }
-        const hashPassword = await bcrypt.hash(password, 5)
-        const user = await User.create({email, role, password: hashPassword})
-        const basket = await Basket.create({userId: user.id})
-        const token = jwt.sign(
-                    {id: user.id, email, role },
-            process.env.SECRET_KEY,
-            {experiesIn:'24h'}
-            )
-        return res.json({token})    
+    async registration(req,res){
+
     }
 
     async login(req,res){
